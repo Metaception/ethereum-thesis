3b0de8702b0ce32d970885ba1d9cd422cabb3b29
Leon Chen <Metaception@users.noreply.github.com>
2016-12-06 10:23:45 -0500

    Optimization for single shifts

--- BigInt/Modulus Comparsion.sol
+++ BigInt/Modulus Comparsion.sol
@@ -70,56 +70,54 @@ contract bigInt
 
     function shift(uint8[2] x, int shft) returns(uint8[2])
     {
-        uint8[2] memory r;
-        uint8 carry =   0;
-        uint8 s;
-
         if (shft == 0)
             return x;
         else if (compa(x, [0, 0]) == 0)
             return x;
         else if (shft > 8 || shft < -8)
             throw;
-        else if (shft > 0)
+            
+        uint8[2] memory r;
+        uint8 carry =   0;
+        uint8 s;
+        uint8 a;
+        uint8 b;
+        
+        if (shft > 0)
         {
             s = uint8(shft);
+            if (s == 1) {
+                a = 2;
+                b = 128;
+            } else {
+                a = 2**s;
+                b = 2**(8-s);
+            }
+            
             for (uint i = 0; i < x.length; ++i)
             {
-                r[i] =  x[i] * 2**s + carry;
-                carry = x[i] / 2**(8-s);
+                r[i] =  x[i] * a + carry;
+                carry = x[i] / b;
             }
         }
         else if (shft < 0)
         {
             s = uint8(shft*-1);
+            if (s == 1) {
+                a = 2;
+                b = 128;
+            } else {
+                a = 2**s;
+                b = 2**(8-s);
+            }
+            
             for (uint j = x.length; j > 0; --j)
             {
                 i =     j - 1;
-                r[i] =  x[i] / 2**s + carry;
-                carry = x[i] * 2**(8-s);
+                r[i] =  x[i] / a + carry;
+                carry = x[i] * b;
             }
         }
-
-        return r;
-    }
-
-    function mod2(uint8[2] x, uint8[2] m) returns(uint8[2])
-    {
-        // Check some special cases
-        if (compa(m, [0, 0]) == 0)
-            throw;
-        if (compa(x, [0, 0]) == 0)
-            return x;
-        if (compa(m, x) == 0)
-            return [0, 0];
-        if (compa(m, x) == 1)
-            return x;
-
-        uint8[2] memory r = x;
-
-        // Subtract
-        while (compa(r, m) != -1)
-            (r,) = sub(r, m);
 
         return r;
     }
@@ -150,30 +148,6 @@ contract bigInt
         {
             (r,) =  sub(r, n);
             n =     shift(n, -1);
-        }
-        return r;
-    }
-
-    function addiMod2(uint8[2] x, uint8[2] y, uint8[2] m) returns(uint8[2])
-    {
-        uint8[2] memory r = [0, 0];
-        uint8 carry;
-
-        (r, carry) =    add(x, y);
-
-        if (carry == 0)
-            r = mod2(r, m);
-        else
-        {
-            uint8[2] memory sum =   [0, 0];
-            uint8[2] memory diff =  [0, 0];
-
-            r =         mod2(r, m);
-            (diff, ) =  sub([255, 255], m);
-            (sum, ) =   add(r, [1, 0]);
-
-            (r, ) =     add(sum, diff);
-            r =         mod2(r, m);
         }
         return r;
     }
